e49a37eb57ceb555231564c480f826531f2d091a
Al-Taie <agprosup@gmail.com>
2022-06-30 19:23:41 +0300

    fix: update app theme

---
+++ presentation/src/main/java/com/altaie/socialdownloader/ui/theme/Size.kt
@@ -0,0 +1,24 @@
+package com.altaie.socialdownloader.ui.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.ReadOnlyComposable
+import androidx.compose.runtime.compositionLocalOf
+import androidx.compose.ui.unit.Dp
+import androidx.compose.ui.unit.dp
+
+data class Size(
+    val default: Dp = 0.dp,
+    val extraSmall: Dp = 4.dp,
+    val small: Dp = 8.dp,
+    val extraMedium: Dp = 12.dp,
+    val medium: Dp = 16.dp,
+    val large: Dp = 32.dp,
+    val extraLarge: Dp = 64.dp,
+)
+
+val LocalSize = compositionLocalOf { Size() }
+val MaterialTheme.size
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalSize.current
--- presentation/src/main/java/com/altaie/socialdownloader/ui/theme/Theme.kt
+++ presentation/src/main/java/com/altaie/socialdownloader/ui/theme/Theme.kt
@@ -3,38 +3,71 @@ package com.altaie.socialdownloader.ui.theme
 import android.app.Activity
 import android.os.Build
 import androidx.compose.foundation.isSystemInDarkTheme
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.darkColorScheme
-import androidx.compose.material3.dynamicDarkColorScheme
-import androidx.compose.material3.dynamicLightColorScheme
-import androidx.compose.material3.lightColorScheme
-import androidx.compose.runtime.Composable
-import androidx.compose.runtime.SideEffect
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
 import androidx.compose.ui.graphics.toArgb
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.platform.LocalView
-import androidx.core.view.ViewCompat
-
-private val DarkColorScheme = darkColorScheme(
-    primary = Purple80,
-    secondary = PurpleGrey80,
-    tertiary = Pink80
-)
+import androidx.core.view.WindowCompat
 
 private val LightColorScheme = lightColorScheme(
-    primary = Purple40,
-    secondary = PurpleGrey40,
-    tertiary = Pink40
+    primary = light_primary,
+    onPrimary = light_onPrimary,
+    primaryContainer = light_primaryContainer,
+    onPrimaryContainer = light_onPrimaryContainer,
+    secondary = light_secondary,
+    onSecondary = light_onSecondary,
+    secondaryContainer = light_secondaryContainer,
+    onSecondaryContainer = light_onSecondaryContainer,
+    tertiary = light_tertiary,
+    onTertiary = light_onTertiary,
+    tertiaryContainer = light_tertiaryContainer,
+    onTertiaryContainer = light_onTertiaryContainer,
+    error = light_error,
+    errorContainer = light_errorContainer,
+    onError = light_onError,
+    onErrorContainer = light_onErrorContainer,
+    background = light_background,
+    onBackground = light_onBackground,
+    surface = light_surface,
+    onSurface = light_onSurface,
+    surfaceVariant = light_surfaceVariant,
+    onSurfaceVariant = light_onSurfaceVariant,
+    outline = light_outline,
+    inverseOnSurface = light_inverseOnSurface,
+    inverseSurface = light_inverseSurface,
+    inversePrimary = light_inversePrimary,
+//	shadow = light_shadow,
+)
 
-    /* Other default colors to override
-    background = Color(0xFFFFFBFE),
-    surface = Color(0xFFFFFBFE),
-    onPrimary = Color.White,
-    onSecondary = Color.White,
-    onTertiary = Color.White,
-    onBackground = Color(0xFF1C1B1F),
-    onSurface = Color(0xFF1C1B1F),
-    */
+private val DarkColorScheme = darkColorScheme(
+    primary = dark_primary,
+    onPrimary = dark_onPrimary,
+    primaryContainer = dark_primaryContainer,
+    onPrimaryContainer = dark_onPrimaryContainer,
+    secondary = dark_secondary,
+    onSecondary = dark_onSecondary,
+    secondaryContainer = dark_secondaryContainer,
+    onSecondaryContainer = dark_onSecondaryContainer,
+    tertiary = dark_tertiary,
+    onTertiary = dark_onTertiary,
+    tertiaryContainer = dark_tertiaryContainer,
+    onTertiaryContainer = dark_onTertiaryContainer,
+    error = dark_error,
+    errorContainer = dark_errorContainer,
+    onError = dark_onError,
+    onErrorContainer = dark_onErrorContainer,
+    background = dark_background,
+    onBackground = dark_onBackground,
+    surface = dark_surface,
+    onSurface = dark_onSurface,
+    surfaceVariant = dark_surfaceVariant,
+    onSurfaceVariant = dark_onSurfaceVariant,
+    outline = dark_outline,
+    inverseOnSurface = dark_inverseOnSurface,
+    inverseSurface = dark_inverseSurface,
+    inversePrimary = dark_inversePrimary,
+//	shadow = dark_shadow,
 )
 
 @Composable
@@ -42,27 +75,50 @@ fun SocialDownloaderTheme(
     darkTheme: Boolean = isSystemInDarkTheme(),
     // Dynamic color is available on Android 12+
     dynamicColor: Boolean = true,
+    forceDarkTheme: Boolean = false,
     content: @Composable () -> Unit
 ) {
     val colorScheme = when {
         dynamicColor && Build.VERSION.SDK_INT >= Build.VERSION_CODES.S -> {
-            val context = LocalContext.current
-            if (darkTheme) dynamicDarkColorScheme(context) else dynamicLightColorScheme(context)
+            with(LocalContext.current) {
+                when {
+                    darkTheme -> dynamicDarkColorScheme(this)
+                    else -> dynamicLightColorScheme(this)
+                }
+            }
         }
-        darkTheme -> DarkColorScheme
+        darkTheme || forceDarkTheme -> DarkColorScheme
         else -> LightColorScheme
     }
+
+    fun MaterialTheme.isDark() = darkTheme || forceDarkTheme
+
     val view = LocalView.current
     if (!view.isInEditMode) {
         SideEffect {
-            (view.context as Activity).window.statusBarColor = colorScheme.primary.toArgb()
-            ViewCompat.getWindowInsetsController(view)?.isAppearanceLightStatusBars = darkTheme
+            (view.context as Activity).window.apply {
+                statusBarColor = colorScheme.primary.toArgb()
+                WindowCompat.getInsetsController(this, view).isAppearanceLightStatusBars = darkTheme
+            }
+        }
+
+        CompositionLocalProvider(
+            LocalSize provides Size(),
+            LocalConfig provides Config(isDarkTheme = darkTheme || forceDarkTheme)
+        ) {
+            MaterialTheme(
+                colorScheme = colorScheme,
+                typography = AppTypography,
+                content = content
+            )
         }
     }
 
-    MaterialTheme(
-        colorScheme = colorScheme,
-        typography = Typography,
-        content = content
-    )
 }
+
+data class Config (val isDarkTheme: Boolean = false)
+val LocalConfig = compositionLocalOf { Config() }
+val MaterialTheme.config
+    @Composable
+    @ReadOnlyComposable
+    get() = LocalConfig.current
