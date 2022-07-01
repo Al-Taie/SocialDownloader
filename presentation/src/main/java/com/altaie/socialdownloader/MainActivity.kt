077e64ed374f607f4a4a016ff4b6ba84036569b0
Al-Taie <agprosup@gmail.com>
2022-07-01 22:18:47 +0300

    fix: enable open when share

--- presentation/src/main/java/com/altaie/socialdownloader/MainActivity.kt
+++ presentation/src/main/java/com/altaie/socialdownloader/MainActivity.kt
@@ -1,7 +1,11 @@
 package com.altaie.socialdownloader
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
@@ -11,6 +15,9 @@ import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.core.content.getSystemService
 import com.altaie.socialdownloader.ui.home.HomeScreen
 import com.altaie.socialdownloader.ui.theme.SocialDownloaderTheme
 import com.altaie.socialdownloader.utils.toUrlOrNull
@@ -22,7 +29,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         val action: String? = intent?.action
-        val data: Uri? = intent?.data
+        var data: Uri? = intent?.data
+
+        intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
+            data = Uri.parse(it)
+        }
 
         setContent {
             SocialDownloaderTheme {
@@ -34,18 +45,38 @@ class MainActivity : ComponentActivity() {
                 }
             }
         }
+        checkPermissions()
+    }
+
+
+    private fun checkPermissions(): Boolean {
+        var result: Int
+        val listPermissionsNeeded: MutableList<String> = ArrayList()
+        val permissions = arrayOf(
+            Manifest.permission.INTERNET,
+            Manifest.permission.ACCESS_NETWORK_STATE,
+            Manifest.permission.READ_EXTERNAL_STORAGE,
+            Manifest.permission.WRITE_EXTERNAL_STORAGE,
+        )
+
+        for (permission in permissions) {
+            result = ContextCompat.checkSelfPermission(this, permission)
+            if (result != PackageManager.PERMISSION_GRANTED) {
+                listPermissionsNeeded.add(permission)
+            }
+        }
+
+        if (listPermissionsNeeded.isNotEmpty()) {
+            ActivityCompat.requestPermissions(this, listPermissionsNeeded.toTypedArray(), 100)
+            return false
+        }
+        return true
     }
 }
 
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
-    SocialDownloaderTheme {
-        Greeting("Android")
-    }
+    SocialDownloaderTheme {}
 }
