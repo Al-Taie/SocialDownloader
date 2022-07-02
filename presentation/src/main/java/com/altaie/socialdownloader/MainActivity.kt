a7cc8ba01395a3ea54857664e42144f1621597e0
Al-Taie <agprosup@gmail.com>
2022-07-02 12:03:46 +0300

    feat: implement splash screen

--- presentation/src/main/java/com/altaie/socialdownloader/MainActivity.kt
+++ presentation/src/main/java/com/altaie/socialdownloader/MainActivity.kt
@@ -15,6 +15,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.altaie.socialdownloader.ui.home.HomeScreen
 import com.altaie.socialdownloader.ui.theme.SocialDownloaderTheme
 import com.altaie.socialdownloader.utils.toUrlOrNull
@@ -24,6 +25,9 @@ import dagger.hilt.android.AndroidEntryPoint
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        installSplashScreen()
+
         var data: Uri? = intent?.data
 
         intent.getStringExtra(Intent.EXTRA_TEXT)?.let {
